f68732831ab8270f8a611d50c5a6013279fbd5bc
Shunchao Wang <shunchao.wang@gmail.com>
2024-04-05 12:33:06 -0400

    feat:try to create a typescript react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios"
+import { FC, useEffect, useState } from "react"
+import User from "./components/User"
+import { AppProps, UserType } from "./App.types"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: FC<AppProps> = ({ title }) => {
+  const [users, setUsers] = useState<UserType[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [username, setUsername] = useState("")
 
+  const getUsers = async () => {
+    try {
+      setIsLoading(true)
+      const { data } = await axios.get("https://randomuser.me/api/?results=10")
+      console.log(data)
+      setUsers(data.results)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  // this will load automatically when page is loaded
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  // with typescript every variable should have a type
+  // i use an inline function to handle the input change event,
+  // the correct type is automatically passed to the function parameter
+  // when using inline functions
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{title}</h1>
+      <input
+        type="text"
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <div>{username}</div>
+      <br />
+      <button onClick={getUsers}>Load Users</button>
+      {isLoading && <p>Loading...</p>}
+      <ul>
+        {users.map(({ login, name, email }) => (
+          <User key={login.uuid} name={name} email={email} />
+        ))}
+      </ul>
+    </div>
   )
 }
+
+// const App = ({ title }: { title: string }) => {
+//   return <div>{title}</div>
+// }
 
 export default App
